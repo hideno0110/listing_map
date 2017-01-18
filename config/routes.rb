c0182ac120cf75d49d214571bf54faa4c6c3d900
Nori Ohara <hideno0110@gmail.com>
2017-01-18 13:50:28 +0900

    authenticate redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'listings/index'
+
+  get 'listings/show'
+
+  get 'listings/new'
+
+  get 'listings/create'
+
+  get 'listings/edit'
+
+  get 'listings/update'
+
   root :to => "pages#index" 
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations =>  'registrations' }
